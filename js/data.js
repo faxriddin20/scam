8cb4b720e21ab377fd83efc9d15db32e9cf7fcf7
faxriddin20 <rx776854@gmail.com>
2023-01-16 11:06:01 +0500

    second

--- js/data.js
+++ js/data.js
@@ -1,3 +1,3 @@
 const examples = [
-    {hello:"Hello",yearsOld:"Yoshing Nechchida",hobby:"Seni nimalar qiziqtiradi",scam:"Seni barcha ma'lumotlaring yozib olindi Hayr )))))",}
+    {scam:"Seni barcha ma'lumotlaring yozib olindi Hayr )))))",}
 ]
